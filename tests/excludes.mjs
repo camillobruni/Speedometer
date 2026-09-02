8b726169408e987ed389601ee1bbad8cdf1dee0e
Camillo Bruni <cbruni@chromium.org>
2026-09-02 11:23:59 +0200

    partial-revert

--- tests/excludes.mjs
+++ tests/excludes.mjs
@@ -2,6 +2,13 @@ export const EXCLUDES = new Set([
     // TODO: Re-enable once packages are updated and node 22 is supported
     "suites/todomvc/architecture-examples/vue",
     "suites/todomvc/architecture-examples/vue-complex",
-    "suites/newssite/news-nuxt",
     "suites/react-stockcharts",
+    "suites/todomvc/architecture-examples/angular",
+    "suites/todomvc/architecture-examples/angular-complex",
+    "suites/newssite/news-next",
+    "suites/editors",
+    "suites/charts",
+    "suites-experimental/javascript-wc-indexeddb",
+    "suites/todomvc/vanilla-examples/javascript-web-components",
+    "suites/todomvc/vanilla-examples/javascript-web-components-complex"
 ]);
