21bb559067117eeae138919ec7986f62369f09b1
Camillo Bruni <cbruni@chromium.org>
2026-09-02 11:35:02 +0200

    formatting

--- tests/excludes.mjs
+++ tests/excludes.mjs
@@ -1,14 +1,14 @@
 export const EXCLUDES = new Set([
     // TODO: Re-enable once packages are updated and node 22 is supported
-    "suites/todomvc/architecture-examples/vue",
-    "suites/todomvc/architecture-examples/vue-complex",
-    "suites/react-stockcharts",
-    "suites/todomvc/architecture-examples/angular",
-    "suites/todomvc/architecture-examples/angular-complex",
-    "suites/newssite/news-next",
-    "suites/editors",
-    "suites/charts",
     "suites-experimental/javascript-wc-indexeddb",
-    "suites/todomvc/vanilla-examples/javascript-web-components",
+    "suites/charts",
+    "suites/editors",
+    "suites/newssite/news-next",
+    "suites/react-stockcharts",
+    "suites/todomvc/architecture-examples/angular-complex",
+    "suites/todomvc/architecture-examples/angular",
+    "suites/todomvc/architecture-examples/vue-complex",
+    "suites/todomvc/architecture-examples/vue",
     "suites/todomvc/vanilla-examples/javascript-web-components-complex",
+    "suites/todomvc/vanilla-examples/javascript-web-components",
 ]);
